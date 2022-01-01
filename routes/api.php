8ee1b7793c182e0bc37d08250a8824993b2767ed
Guilherme-Ferreti <guiferretis@gmail.com>
2022-01-01 14:25:08 -0300

    Refactored API routes file

--- routes/api.php
+++ routes/api.php
@@ -15,15 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['prefix' => '/auth', 'as' => 'auth.'], function () {
-    Route::post('/register', [AuthController::class, 'register'])->name('register');
-    Route::post('/login', [AuthController::class, 'login'])->name('login');
-
-    Route::middleware('auth:sanctum')->group(function () {
-        Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-    });
-});
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('auth')
+    ->name('auth.')
+    ->group(function () {
+        Route::post('/register', [AuthController::class, 'register'])->name('register');
+        Route::post('/login', [AuthController::class, 'login'])->name('login');
+        Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth:sanctum');
 });
